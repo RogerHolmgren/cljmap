3a561e0fcfaab7927faf341a00042b1ff7d101b2
RogerH <rogerholmgren@gmail.com>
2023-04-23 19:42:29 +0200

    Tiny refactoring

--- src/cljmap/views.cljs
+++ src/cljmap/views.cljs
@@ -14,13 +14,10 @@
 (defn to-prop-list [prop]
   ( let [k (key prop)
          v (val prop)]
-    ; TODO: Handle 'estimated-effects'
-    ; (.log js/console (str "V: " v " is map? " (map? v)))
     [:li {:key k} (str k ": ") (val-or-popup v)]
     ))
 
 (defn display-features [{my-type :type geo :geometry props :properties}]
-  ; (.log js/console (str "Props: " (get-props props)))
   [:div {:key (:name props)}
    [:p (str "Type: " my-type)]
    [:p (str "geo-type: " (:type geo))]
@@ -33,11 +30,8 @@
     [:div
      [:h1 "Geo data title"]
      [gmap-wrapper]
-     [:div 
-      ; (.log js/console (str "Data: >>>>> " (:features @data)))
-      (map display-features (:features @data))
-      [:button {:on-click #(rf/dispatch [::events/fetch-geodata])} "Get geo-data"]
-      ]
+     (map display-features (:features @data))
+     [:button {:on-click #(rf/dispatch [::events/fetch-geodata])} "Get geo-data"]
      ]))
 
 (comment
@@ -45,3 +39,5 @@
   (if true (val-or-popup "aueo") "bla")
   (map to-prop-list {:key1 "val1" :key2 {:a "a"}})
   )
+
+; (.log js/console (str "x"))
