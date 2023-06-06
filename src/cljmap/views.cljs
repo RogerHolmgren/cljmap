a4e96b1376e66ae42b917ef4c23c7ae7007c099e
RogerH <rogerholmgren@gmail.com>
2023-06-06 22:26:38 +0200

    Clean some test code

--- src/cljmap/views.cljs
+++ src/cljmap/views.cljs
@@ -5,17 +5,6 @@
     [cljmap.subs :as subs]
     [re-frame.core :as rf]))
 
-(defn val-or-popup [v]
-  (if (map? v)
-    [:a "get details"]
-    (str v)))
-
-(defn to-prop-list [prop]
-  ( let [k (key prop)
-         v (val prop)]
-    [:li {:key k} (str k ": ") (val-or-popup v)]
-    ))
-
 (defn feature-list []
   (let [{:keys [ mapData my-filter ]} @(rf/subscribe [::subs/mapFeatures])]
     [:div
@@ -39,9 +28,4 @@
     [:button.button {:on-click #(rf/dispatch [::events/put-filter 8])} "Simple filter 8"]
     ]
    ])
-
-(comment
-  (if true (val-or-popup "aueo") "bla")
-  (map to-prop-list {:key1 "val1" :key2 {:a "a"}})
-  )
 
